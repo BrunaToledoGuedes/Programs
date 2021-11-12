58e3977a9f3665fa487c6e8cb82009fbe9d6dc91
BrunaToledoGuedes <brunatoledo30@gmail.com>
2021-11-12 12:56:05 -0300

    Update simulador.bat

--- Simulador/simulador.bat
+++ Simulador/simulador.bat
@@ -1,2 +1,2 @@
 python simulador.py -n 200 -g 5 -s 1 -W 280 -H 280 -v 0 >log200ncolisao.txt
-python SimParser.py -f log200ncolisao.txt >\python27\RAW-200nos-5grupos-280metros_s1.txt
+python SimParser.py -f log200ncolisao.txt >RAW-200nos-5grupos-280metros_s1.txt
